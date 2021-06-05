3f6a6a6dee8bd1c5227d7c91650238fcae244239
Zi-on <zioniflores12@yahoo.com>
2021-06-05 12:13:23 -0500

    added news api to script, also added moment js to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("https://dashboard.nbshare.io/api/v1/apps/reddit")
+var newsApi = 'http://api.mediastack.com/v1/news?access_key=126705cb9c518744d6816588b7be6b11&languages=en&keywords=BTC';
+
+
+fetch(newsApi, {
+
+})
+.then (function(response) {
+    return response.json()
+})
+.then (function(data) {
+   console.log(data)
+   console.log(data.data[0].title)
+   console.log(data.data[0].description)
+   console.log(data.data[0].url)
+   console.log(data.data[5].title)
+   console.log(data.data[5].description)
+   console.log(data.data[5].url)
+  
+
+
+})
+
+
