403e2861e9c672b3730becc2dad431841329fe04
Billy Dorman <b.d.dorman12@gmail.com>
2021-06-10 22:20:15 -0500

    up to date

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ function makeRandomArray(){
             }
         } 
         randomArray.push(newNewRandom);
-        console.log(randomArray+"wtf"); 
+        console.log(randomArray); 
     }
 };
 makeRandomArray();
@@ -31,9 +31,18 @@ fetch(stockApi, {
 })
 .then(function(data) {
     console.log(data)
+    for (var i = 0; i < 5; i++){
+        var name = data.data[i].name;
+        var ranking = data.data[i].rank;
+        var price = data.data[i].priceUsd;
+        var symbol = data.data[i].symbol;
+        console.log(name, ranking, Math.round(price *1000)/1000, symbol)
+
+
+    }
     for( x = 0; x < randomArray.length; x++){
         console.log(data.data[randomArray[x]].name);
-        var newNewsApi = "http://api.mediastack.com/v1/news?access_key=126705cb9c518744d6816588b7be6b11&languages=en&keywords=" + data.data[randomArray[x]].name;
+        var newNewsApi = "https://api.currentsapi.services/v1/search?keywords=" +data.data[randomArray[x]].name+ "&language=en&apiKey=k6P8Em4qB8ukRQLGTafAvMDafmfTEUTmUeYB-tstXbZM_Xfy";
         fetch(newNewsApi, {
 
         })
@@ -79,7 +88,7 @@ fetch(stockApi, {
 findBtn.on("click", function (event) {
     
     var input = inputArea.val().trim();
-    var newsApiUrl = 'http://api.mediastack.com/v1/news?access_key=126705cb9c518744d6816588b7be6b11&languages=en&keywords='+input;
+    var newsApiUrl = "https://api.currentsapi.services/v1/search?keywords=" +input+ "&language=en&apiKey=k6P8Em4qB8ukRQLGTafAvMDafmfTEUTmUeYB-tstXbZM_Xfy";
     console.log(newsApiUrl)
 
     fetch(newsApiUrl, {
